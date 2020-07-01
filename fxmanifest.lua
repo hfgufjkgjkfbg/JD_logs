eb644b4c68d774733468247d123cc47b60d4a086
JokeDevil <49778541+JokeDevil@users.noreply.github.com>
2020-07-01 16:24:53 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 author 'JokeDevil'
 description 'FXServer logs to Discord'
-version '1.0.0'
+version '1.0.1'
 
 
-server_script 'server.lua'
-client_script 'client.lua'
+server_script 'server/server.lua'
+client_script 'client/client.lua'
