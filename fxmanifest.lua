29edc0b2abb3f604448651bdb8ce178f0c81161a
JokeDevil <49778541+JokeDevil@users.noreply.github.com>
2020-07-01 22:33:34 +0200

    Update fxmanifest.lua
    added game and fx_version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,11 @@
 author 'JokeDevil'
 description 'FXServer logs to Discord'
 version '1.0.1'
+url 'https://jokedevil.com'
 
 
 server_script 'server/server.lua'
 client_script 'client/client.lua'
+
+game 'gta5'
+fx_version 'bodacious'
