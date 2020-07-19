9aaa49a2ac4219f26bc95d44dd211d916c70814b
JokeDevil <49778541+JokeDevil@users.noreply.github.com>
2020-07-19 22:42:58 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -29,19 +29,15 @@ Config.resourceColor = "15461951"	-- Resource Stopped/Started
 
 
 Config.webhooks = {
-	all = "<DISCORD_WEBHOOK>",
-	chat = "<DISCORD_WEBHOOK>",
-	joins = "<DISCORD_WEBHOOK>",
-	leaving = "<DISCORD_WEBHOOK>",
-	deaths = "<DISCORD_WEBHOOK>",
-	shooting = "<DISCORD_WEBHOOK>",
-	resources = "<DISCORD_WEBHOOK>",
+	all = "DISCORD_WEBHOOK",
+	chat = "DISCORD_WEBHOOK",
+	joins = "DISCORD_WEBHOOK",
+	leaving = "DISCORD_WEBHOOK",
+	deaths = "DISCORD_WEBHOOK",
+	shooting = "DISCORD_WEBHOOK",
+	resources = "DISCORD_WEBHOOK",
 
-  -- You can add more logs by using exports in other resources
-  -- When the action is done call the function below in the script to send the information to JD_logs
-  -- exports.JD_logs:discord('<MESSAGE_YOU_WANT_TO_POST_IN_THE_EMBED>', '1752220', '<WEBHOOK_CHANNEL>')
-  -- Then create a webhook for the action you just executed
-  -- <YOUR NEW WEBHOOK NAME> = "<DISCORD_WEBHOOK>",
+  -- How you add more logs is explained on https://docs.jokedevil.com/JD_logs
   }
 
 
